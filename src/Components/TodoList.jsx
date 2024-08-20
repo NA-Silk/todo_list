c3ab972d6f2a4379350ada6671a970d09fac9b18
NA-Silk <nicolasseda@gmail.com>
2024-08-20 01:47:19 -0400

    added more list functionality

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -7,9 +7,19 @@ const [headingInput, setHeadingInput] = useState('')
 const [listInputs, setListInputs] = useState({})
 const handleAddTodo = () => {
     if (headingInput.trim() !== '') {
-    setTodos([...todos, {heading: headingInput, lists: []}]);
-    setHeadingInput('');
+        setTodos([...todos, {heading: headingInput, lists: []}]);
+        setHeadingInput('');
 }};
+const handleAddList = (index) => {
+    if(listInputs[index] && listInputs[index].trim() !== '') {
+        const newTodos = [...todos];
+        newTodos[index].lists.push(listInputs[index]);
+        setTodos(newTodos);
+        setListInputs({...listInputs, [index]: ''});
+}};
+const handleListInputChange = (index, value) => {
+    setListInputs({...listInputs, [index]: value});
+};
 
 return (
     <>
@@ -32,6 +42,23 @@ return (
                     <div className='heading_todo'>
                         <h3>{todo.heading}</h3>
                         <button className='delete-button-heading'>Delete Heading</button>
+                        <div className='add_list'>
+                            <input
+                                type='text'
+                                className='list-input'
+                                placeholder='Add List'
+                                value={listInputs[index] || ''}
+                                onChange={(e) => handleListInputChange(index, e.target.value)}
+                            />
+                            <button className='add-list-button' onClick={() => handleAddList(index)}>Add List</button>
+                            <ul>
+                                {todo.lists.map((list, listIndex) => (
+                                    <li key={listIndex} className='todo_inside_list'>
+                                        <p>{list}</p>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
                     </div>
                 </div>
             ))}
